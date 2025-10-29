ef887566efa51dce36a1e1f0e326e0c9b27a239a
CarlosEspEsc <cespinosaescrig@gmail.com>
2025-10-29 19:02:21 +0100

    test de funcion fibonacci hecha

--- test/ejercicio.test.js
+++ test/ejercicio.test.js
@@ -1,1 +1,12 @@
 import { fibonacci, esPinValido } from '../src/ejercicio.js'
+import { describe, it, expect } from 'vitest'
+
+describe('ejercicio'), () => {
+    it('fibonacci', () => {
+        expect(fibonacci(7)).toBe(11)
+        expect(fibonacci(5)).toBe(7)
+        expect(fibonacci(0)).toBe(0)
+        expect(fibonacci(1)).toBe(1)
+        expect(fibonacci(-1)).toThrow(RangeError)
+    })
+}
